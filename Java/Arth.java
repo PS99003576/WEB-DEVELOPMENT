14da1f6dddab785fee3e44b9ca23e5969c07e99f
PS99003576 <roshanzameer.shaik@ltts.com>
2021-02-25 00:40:36 +0530

    updates

--- Java/Arth.java
+++
@@ -1,5 +0,0 @@
-import java.util.*;
-class Arithmetic{
-public static void main(String args[])
-}
-
--- Java/LeapYear.java
+++ Java/LeapYear.java
@@ -1,37 +1,27 @@
 import java.util.*;
 class LeapYear{
     int check_Leap_Year(int year){
-
-        if(year<0)
-            return -1;
-
-        else if(year%4 == 0)
+        if(year<0) return -1;    //invalid input
+        else if(year%4 == 0)     
         {
             if(year%100 == 0)
             {
-                if(year%400 == 0)
-                    return 1;
-                else   
-                    return 0;
-            }
-            return 1;
-        }
-        else
-            return 0;
-
-    
-
+                if(year%400 == 0) return 1;
+                else return 0;
+            }return 1;
+        }else return 0;
     }
 
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Year : ");
-        int year = sc.nextInt();
+        int year = sc.nextInt(); // User Input 
 
-        LeapYear leap_object = new LeapYear();
+        LeapYear leap_object = new LeapYear(); //Object Creation
         
-        int return_value = leap_object.check_Leap_Year(year);
+        int return_value = leap_object.check_Leap_Year(year); // Function Call 
+
         switch (return_value) {
             case 1:
                 System.out.println("Yes");
@@ -47,6 +37,4 @@ class LeapYear{
         sc.close();
     }
 
-    
-    
 }
